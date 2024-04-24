693d67532643054f1a05956a7e395d1d05195e02
João Victor Santos Sampaio <47907262+PinkJoao@users.noreply.github.com>
2024-04-24 10:42:23 -0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def pertence_fibonacci(numero):
+    a, b = 0, 1
+    while b < numero:
+        a, b = b, a + b
+    if b == numero:
+        return True
+    else:
+        return False
+
+numero = int(input("Digite um número: "))
+if verifica_fibonacci(numero):
+    print(numero, "pertence à sequência de Fibonacci.")
+else:
+    print(numero, "não pertence à sequência de Fibonacci.")
